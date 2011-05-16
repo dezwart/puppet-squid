9359cb6e492618bcb8c51ae07ab5d3b434d77f1b
Pete de Zwart <dezwart@gmail.com>
2011-05-16 22:10:10 +1000

    Fleshing out the squid sub-module. Struggling with puppet apply test/init.pp, so that has been skipped.

---
+++ manifests/init.pp
@@ -0,0 +1,26 @@
+class squid {
+	package { 'squid3':
+		ensure	=> installed,
+	}
+
+	package { 'adzapper':
+		ensure	=> installed,
+		require	=> Package['squid3'],
+	}
+
+	file { '/etc/squid3/squid.conf':
+		ensure	=> file,
+		owner	=> root,
+		group	=> root,
+		mode	=> '0644',
+		comtent	=> template('squid/squid.conf.erb'),
+		require => Package['squid3'],
+	}
+
+	service { 'squid3':
+		ensure		=> running,
+		enable		=> true,
+		require		=> Package['squid3'],
+		subscribe	=> File['/etc/squid3/squid.conf'],
+	}
+}
