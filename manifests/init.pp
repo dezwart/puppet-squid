db85127e06459515083b226fe49cc6776fe82370
Pete de Zwart <pdzwart@atlassian.com>
2012-06-26 15:00:18 +1000

    PERF-258: Working around new Debian squid3 configuration where the default cache_dir is commented out so is not initialised during deb installation via squid3 -z.

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,7 @@ class squid( $localnet_src = '10.0.0.0/8',
     $maximum_object_size_in_memory = '512 KB',
     $memory_replacement_policy = 'lru',
     $cache_replacement_policy = 'lru',
+    $cache_dir = '/var/spool/squid3',
     $cache_dir_type = 'ufs',
     $cache_dir_size = 100,
     $maximum_object_size = '4096 KB',
@@ -13,30 +14,51 @@ class squid( $localnet_src = '10.0.0.0/8',
     $visible_hostname = undef,
     $snmp_port = undef,
     $adzapper = false ) {
-    package { 'squid3':
+
+    $package = 'squid3'
+    $service = 'squid3'
+    $user = 'proxy'
+    $group = 'proxy'
+
+    package { $package:
         ensure  => installed,
     }
 
     if $adzapper {
         package { 'adzapper':
             ensure  => installed,
-            require => Package['squid3'],
+            require => Package[$package],
         }
     }
 
-    file { '/etc/squid3/squid.conf':
+    file { "/etc/$package/squid.conf":
         ensure  => file,
         owner   => root,
         group   => root,
         mode    => '0644',
         content => template('squid/squid.conf.erb'),
-        require => Package['squid3'],
+        require => Package[$package],
     }
 
-    service { 'squid3':
+    file { $cache_dir:
+        ensure  => directory,
+        owner   => $user,
+        group   => $group,
+        mode    => '0755',
+        require => Package[$package],
+    }
+
+    exec { 'Init cache dir':
+        command => "service $service stop && $service -z"
+        path    => "/usr/sbin",
+        creates => "$cache_dir/00",
+        notify  => Service[$service],
+    }
+
+    service { $service:
         ensure      => running,
         enable      => true,
-        require     => Package['squid3'],
+        require     => Package[$package],
         restart     => '/etc/init.d/squid3 reload',
         subscribe   => File['/etc/squid3/squid.conf'],
     }
